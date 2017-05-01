df10ec7b043dc30a6493dc199999262a37b09b0b
tkoar <terrancekoar@gmail.com>
2017-04-30 22:48:27 -0400

    done.

---
+++ lib/backer.rb
@@ -0,0 +1,16 @@
+class Backer
+  attr_accessor :name, :backed_projects
+
+
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(project)
+    self.backed_projects << project
+    project.backers << self
+  end
+
+end
